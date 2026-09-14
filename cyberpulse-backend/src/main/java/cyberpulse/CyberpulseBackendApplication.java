7fed643efc13b2907d55aa4e0ab7a865428b6deb
kumarmanish562 <qmanishkumar562@gmail.com>
2026-09-14 17:00:49 +0530

    chore: initialize backend project structure with Maven and Spring Boot configuration

---
+++ cyberpulse-backend/src/main/java/cyberpulse/CyberpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package cyberpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberpulseBackendApplication.class, args);
+	}
+
+}
---
+++ cyberpulse-backend/src/test/java/cyberpulse/CyberpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cyberpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
